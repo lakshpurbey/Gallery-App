8ac48acb34304406776c0c2c906969eda8cf1c9a
sam purbey <sambabu.purbey@gmail.com>
2017-01-25 11:05:37 +0545

    Initial Commit

---
+++ Gallery App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery App
+//
+//  Created by New User on 1/25/17.
+//  Copyright © 2017 Bidhee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
